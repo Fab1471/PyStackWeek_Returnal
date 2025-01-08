93593404c22d456dc5e7dc596f124311993a3053
Fabri <ism.fabri@gmail.com>
2025-01-08 20:07:04 -0300

    0.00

---
+++ views/view.py
@@ -0,0 +1,49 @@
+import __init__
+from models.database import engine
+from models.model import Subscription, Payments
+from sqlmodel import Session, select
+from datetime import date
+
+class SubscriptionService:
+    def __init__(self, engine):
+        self.engine = engine
+
+    def create(self, subscription: Subscription):
+        with Session(self.engine) as session:
+            session.add(subscription)
+            session.commit()
+            return subscription
+        
+    def list_all(self):
+        with Session(self.engine) as session:
+            statement = select(Subscription)
+            results = session.exec(statement).all()
+        return results
+
+    def _has_pay(self, results):
+        for result in results:
+            if result.date.month == date.today().month:
+                return True
+        return False
+
+    def pay(self, subscription: Subscription):
+        with Session(self.engine) as session:
+            statement = select(Payments).join(Subscription).where(Subscription.empresa==subscription.empresa)
+            results = session.exec(statement).all()
+            print(results)
+            if self._has_pay(results):
+                question = input('Essa conta já foi paga esse mês, deseja pagar novamente? Y ou N: ')
+
+                if not question.upper() == 'Y':
+                    return
+            
+            pay = Payments(subscription_id=subscription.id, date=date.today())
+            session.add(pay)
+            session.commit()
+
+
+ss = SubscriptionService(engine)
+
+assinaturas = ss.list_all()
+for i, s in enumerate(assinaturas):
+    print(f'[{i}] -> {s.empresa}')
