0b9f734a3eb1ad7e347bfc98161e5ce9b775ffe7
Fabri <ism.fabri@gmail.com>
2025-01-13 00:26:29 -0300

    13/01/2024 - 12:26 a.m

--- Psw_Returnal - Aula 2/diario/views.py
+++ Psw_Returnal - Aula 2/diario/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from . models import Pessoa
 
 # Create your views here.
 def home(request):
@@ -14,3 +15,17 @@ def escrever(request):
         pessoas = request.POST.getlist('pessoas')
         texto = request.POST.get('texto')
         return HttpResponse(f'{titulo} - {tags} - {pessoas} - {texto}')
+
+def cadastrar_pessoa(request):
+    if request.method == 'GET':
+        return render(request, 'pessoa.html')
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        foto = request.FILES.get('foto')
+        
+        pessoa = Pessoa(        # a varíavel pessoa herda da Classe Pessoa que possui os 'atributos'? de nome e foto
+            nome=nome,          # o primeiro é a varíavel da classe Pessoa o da esquerda e o da direita é o input que vai vir no form
+            foto=foto           # o segundo é referente a variavel da função cadastrar_pessoa que vai ser recebido pelo input do form
+        )
+        pessoa.save()
+        return redirect('escrever')
