203ea5b6121b153e8e232dc53cbea25accde3732
Fabri <ism.fabri@gmail.com>
2025-01-14 03:32:53 -0300

    break

--- Psw_Returnal - Aula 3/shortener/models.py
+++ Psw_Returnal - Aula 3/shortener/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Links(models.Model):
+    redirect_link = models.URLField()
+    token = models.CharField(max_length=12, unique=True, null=True, blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    expiration_time = models.DurationField(null=True, blank=True)
+    max_uniques_cliques = models.PositiveIntegerField(null=True, blank=True)
+    active = models.BooleanField(default=True)
